faf7332a06f46f478899595b876646ff0743f449
gustindvr <gustindvr@gmail.com>
2025-12-09 17:43:59 -0300

    generate pwa

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   reactCompiler: true,
+  // tu otras configuraciones...
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
+})(nextConfig);
